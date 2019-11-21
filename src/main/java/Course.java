63223bd7881d4e47b9dfb1754c6a6b477b657c55
AaronF97 <aaronforde97@hotmail.com>
2019-11-21 14:08:23 +0000

    getter and setter Methods

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,3 +1,70 @@
-public class CourseProgramme {
+import org.joda.time.LocalDate;
 
+import java.util.ArrayList;
+
+public class Course {
+
+    private String courseName;
+    private ArrayList<Student> enrolledStudents = new ArrayList<Student>();
+    private ArrayList<Module> courseModules = new ArrayList<Module>();
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Course(String courseName, LocalDate startDate, LocalDate endDate) {
+        this.courseName = courseName;
+        this.endDate = endDate;
+        this.startDate = startDate;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public ArrayList<Module> getCourseModules() {
+        return courseModules;
+    }
+
+    public void setCourseModules(ArrayList<Module> courseModules) {
+        this.courseModules = courseModules;
+    }
+
+    public void addCourseModule(Module m)
+    {
+        courseModules.add(m);
+    }
+
+    public void addStudent(Student student){
+        if(!enrolledStudents.contains(student)){
+            enrolledStudents.add(student);
+
+        }
+    }
+
+    public ArrayList<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public void setEnrolledStudents(ArrayList<Student> enrolledStudents) {
+        this.enrolledStudents = enrolledStudents;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
 }
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,47 +1,63 @@
-import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import java.util.ArrayList;
 
-import java.util.List;
 
 
 public class Student {
 
-    private String name, age, id;
-    private DateTime dob;
-    private List registeredModules;
+    private String name, id, userName;
+    private LocalDate dob;
+    private int age;
+    private ArrayList<Module> modules;
 
-    public Student(String name, String age, DateTime dob, List modules)
+    public Student(String name, String id, LocalDate dob, ArrayList<Module> modules)
     {
         this. name = name;
-        this.age = age;
+        this.age = this.getAge();
         this.dob = dob;
-        this.registeredModules = modules;
+        this.id = id;
+        this.userName = this.getUsername();
+        this.modules = modules;
     }
 
     public int getAge()
     {
-
+        LocalDate currentYear = new LocalDate();
+       int year = currentYear.getYear();
+       return this.age = year - dob.getYear();
     }
-
-    public String getName()
-    {
-        return this.name;
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
-    public String getAge()
-    {
-        return this.age;
-    }
-
-    public String getUsername(String name, String age)
+    public String getUsername()
     {
         String userName;
         userName = name + age;
         return userName;
     }
 
+    public void addModule(Module newModule){
+        modules.add(newModule);
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId() {
+        this.id = id;
+    }
 }
